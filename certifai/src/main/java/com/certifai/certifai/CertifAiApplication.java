d4b23966013bfec6261fa7df211228887fbdd129
unknown <davialealves@gmail.com>
2025-09-02 17:48:19 -0300

    First commit

---
+++ certifai/src/main/java/com/certifai/certifai/CertifAiApplication.java
@@ -0,0 +1,13 @@
+package com.certifai.certifai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifAiApplication.class, args);
+	}
+
+}
---
+++ certifai/src/test/java/com/certifai/certifai/CertifAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certifai.certifai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertifAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
